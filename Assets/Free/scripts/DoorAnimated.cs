42783b62e9114c2674bf01439e4117975dcf3569
bizsel <79521088+bizsel@users.noreply.github.com>
2022-06-06 19:58:11 +0300

    DoorsCompletedMagneticUpgradeAssetDone

---
+++ Assets/Free/scripts/DoorAnimated.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimated : MonoBehaviour
+{
+    private Animator _animator;
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void OpenDoor()
+    {
+        _animator.SetBool("Open",true);
+        
+    }
+
+    public void CloseDoor()
+    {
+        _animator.SetBool("Open",false);
+    }
+}
